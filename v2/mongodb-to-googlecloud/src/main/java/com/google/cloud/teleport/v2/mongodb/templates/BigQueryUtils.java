9b35dbd66d2324474c07e610822ba8eb40f52eea
Jethro Ordaneza <jethro@bookipi.com>
2024-06-27 14:28:55 +0800

    chore: fix format

--- v2/mongodb-to-googlecloud/src/main/java/com/google/cloud/teleport/v2/mongodb/templates/BigQueryUtils.java
+++ v2/mongodb-to-googlecloud/src/main/java/com/google/cloud/teleport/v2/mongodb/templates/BigQueryUtils.java
@@ -1,65 +1,80 @@
+/*
+ * Copyright (C) 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.google.cloud.teleport.v2.mongodb.templates;
 
 import com.google.api.services.bigquery.model.TableRow;
-import org.bson.Document;
-
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import org.bson.Document;
 
 public class BigQueryUtils implements Serializable {
-    static final DateTimeFormatter TIMEFORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+  static final DateTimeFormatter TIMEFORMAT =
+      DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
 
-    public static TableRow getTableSchema(Document document) {
-        TableRow row = new TableRow();
+  public static TableRow getTableSchema(Document document) {
+    TableRow row = new TableRow();
 
-        document.forEach((key, value) -> {
-            if (value == null) {
-                return;
-            }
+    document.forEach(
+        (key, value) -> {
+          if (value == null) {
+            return;
+          }
 
-            String valueClass = value.getClass().getName();
+          String valueClass = value.getClass().getName();
 
-            switch (valueClass) {
-                case "java.lang.Double":
-                case "java.lang.Integer":
-                case "java.lang.Long":
-                case "java.lang.Boolean":
-                case "java.lang.Float":
-                    row.set(key, value);
-                    break;
-                case "org.bson.Document":
-                    Document subDoc = Document.parse(value.toString());
-                    TableRow data = getTableSchema(subDoc);
-                    row.set(key, data);
-                    break;
-                case "java.util.ArrayList":
-                case "java.util.List":
-                    List<?> valueList = (List<?>) value;
-                    List<Object> array = new ArrayList<>();
-                    for (Object item : valueList) {
-                        if (item instanceof Document) {
-                            Document subDocItem = Document.parse(item.toString());
-                            TableRow dataItem = getTableSchema(subDocItem);
-                            array.add(dataItem);
-                        } else {
-                            array.add(item);
-                        }
-                    }
-                    row.set(key, array);
-                    break;
-                default:
-                    row.set(key, value.toString());
-            }
-
+          switch (valueClass) {
+            case "java.lang.Double":
+            case "java.lang.Integer":
+            case "java.lang.Long":
+            case "java.lang.Boolean":
+            case "java.lang.Float":
+              row.set(key, value);
+              break;
+            case "org.bson.Document":
+              Document subDoc = Document.parse(value.toString());
+              TableRow data = getTableSchema(subDoc);
+              row.set(key, data);
+              break;
+            case "java.util.ArrayList":
+            case "java.util.List":
+              List<?> valueList = (List<?>) value;
+              List<Object> array = new ArrayList<>();
+              for (Object item : valueList) {
+                if (item instanceof Document) {
+                  Document subDocItem = Document.parse(item.toString());
+                  TableRow dataItem = getTableSchema(subDocItem);
+                  array.add(dataItem);
+                } else {
+                  array.add(item);
+                }
+              }
+              row.set(key, array);
+              break;
+            default:
+              row.set(key, value.toString());
+          }
         });
 
-        LocalDateTime localdate = LocalDateTime.now(ZoneId.of("UTC"));
-        row.set("timestamp", localdate.format(TIMEFORMAT));
+    LocalDateTime localdate = LocalDateTime.now(ZoneId.of("UTC"));
+    row.set("timestamp", localdate.format(TIMEFORMAT));
 
-        return row;
-    }
+    return row;
+  }
 }
--- v2/mongodb-to-googlecloud/src/main/java/com/google/cloud/teleport/v2/mongodb/templates/MongoDbToBigQueryCdcCustom.java
+++ v2/mongodb-to-googlecloud/src/main/java/com/google/cloud/teleport/v2/mongodb/templates/MongoDbToBigQueryCdcCustom.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.google.cloud.teleport.v2.mongodb.templates;
 
 import com.google.api.services.bigquery.model.TableRow;
@@ -24,127 +39,126 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Template(
-        name = "MongoDB_to_BigQuery_CDC_Custom",
-        category = TemplateCategory.STREAMING,
-        displayName = "MongoDB to BigQuery (CDC) Custom",
-        description =
-                "The MongoDB to BigQuery CDC (Change Data Capture) template is a streaming pipeline that works together with MongoDB change streams. "
-                        + "The pipeline reads the JSON records pushed to Pub/Sub via a MongoDB change stream and writes them to BigQuery.",
-        optionsClass = MongoDbToBigQueryCdc.Options.class,
-        flexContainerName = "mongodb-to-bigquery-cdc",
-        documentation =
-                "https://cloud.google.com/dataflow/docs/guides/templates/provided/mongodb-change-stream-to-bigquery",
-        contactInformation = "https://cloud.google.com/support",
-        preview = true,
-        requirements = {
-                "The target BigQuery dataset must exist.",
-                "The change stream pushing changes from MongoDB to Pub/Sub should be running."
-        },
-        streaming = true,
-        supportsAtLeastOnce = true)
+    name = "MongoDB_to_BigQuery_CDC_Custom",
+    category = TemplateCategory.STREAMING,
+    displayName = "MongoDB to BigQuery (CDC) Custom",
+    description =
+        "The MongoDB to BigQuery CDC (Change Data Capture) template is a streaming pipeline that works together with MongoDB change streams. "
+            + "The pipeline reads the JSON records pushed to Pub/Sub via a MongoDB change stream and writes them to BigQuery.",
+    optionsClass = MongoDbToBigQueryCdc.Options.class,
+    flexContainerName = "mongodb-to-bigquery-cdc",
+    documentation =
+        "https://cloud.google.com/dataflow/docs/guides/templates/provided/mongodb-change-stream-to-bigquery",
+    contactInformation = "https://cloud.google.com/support",
+    preview = true,
+    requirements = {
+      "The target BigQuery dataset must exist.",
+      "The change stream pushing changes from MongoDB to Pub/Sub should be running."
+    },
+    streaming = true,
+    supportsAtLeastOnce = true)
 public class MongoDbToBigQueryCdcCustom {
 
-    private static final Logger LOG = LoggerFactory.getLogger(MongoDbToBigQuery.class);
+  private static final Logger LOG = LoggerFactory.getLogger(MongoDbToBigQuery.class);
 
-    /** Options interface. */
-    public interface Options
-            extends PipelineOptions,
-            MongoDbToBigQueryOptions.MongoDbOptions,
-            MongoDbToBigQueryOptions.PubSubOptions,
-            MongoDbToBigQueryOptions.BigQueryWriteOptions,
-            MongoDbToBigQueryOptions.JavascriptDocumentTransformerOptions,
-            BigQueryStorageApiStreamingOptions {
+  /** Options interface. */
+  public interface Options
+      extends PipelineOptions,
+          MongoDbToBigQueryOptions.MongoDbOptions,
+          MongoDbToBigQueryOptions.PubSubOptions,
+          MongoDbToBigQueryOptions.BigQueryWriteOptions,
+          MongoDbToBigQueryOptions.JavascriptDocumentTransformerOptions,
+          BigQueryStorageApiStreamingOptions {
 
-        // Hide the UseStorageWriteApiAtLeastOnce in the UI, because it will automatically be turned
-        // on when pipeline is running on ALO mode and using the Storage Write API
-        @TemplateParameter.Boolean(
-                order = 1,
-                optional = true,
-                parentName = "useStorageWriteApi",
-                parentTriggerValues = {"true"},
-                description = "Use at at-least-once semantics in BigQuery Storage Write API",
-                helpText =
-                        "When using the Storage Write API, specifies the write semantics. To"
-                                + " use at-least-once semantics (https://beam.apache.org/documentation/io/built-in/google-bigquery/#at-least-once-semantics), set this parameter to `true`. To use exactly-"
-                                + " once semantics, set the parameter to `false`. This parameter applies only when"
-                                + " `useStorageWriteApi` is `true`. The default value is `false`.",
-                hiddenUi = true)
-        @Default.Boolean(false)
-        @Override
-        Boolean getUseStorageWriteApiAtLeastOnce();
+    // Hide the UseStorageWriteApiAtLeastOnce in the UI, because it will automatically be turned
+    // on when pipeline is running on ALO mode and using the Storage Write API
+    @TemplateParameter.Boolean(
+        order = 1,
+        optional = true,
+        parentName = "useStorageWriteApi",
+        parentTriggerValues = {"true"},
+        description = "Use at at-least-once semantics in BigQuery Storage Write API",
+        helpText =
+            "When using the Storage Write API, specifies the write semantics. To"
+                + " use at-least-once semantics (https://beam.apache.org/documentation/io/built-in/google-bigquery/#at-least-once-semantics), set this parameter to `true`. To use exactly-"
+                + " once semantics, set the parameter to `false`. This parameter applies only when"
+                + " `useStorageWriteApi` is `true`. The default value is `false`.",
+        hiddenUi = true)
+    @Default.Boolean(false)
+    @Override
+    Boolean getUseStorageWriteApiAtLeastOnce();
 
-        void setUseStorageWriteApiAtLeastOnce(Boolean value);
+    void setUseStorageWriteApiAtLeastOnce(Boolean value);
+  }
+
+  /** class ParseAsDocumentsFn. */
+  private static class ParseAsDocumentsFn extends DoFn<String, Document> {
+
+    @ProcessElement
+    public void processElement(ProcessContext context) {
+      context.output(Document.parse(context.element()));
     }
+  }
 
-    /** class ParseAsDocumentsFn. */
-    private static class ParseAsDocumentsFn extends DoFn<String, Document> {
+  /**
+   * Main entry point for pipeline execution.
+   *
+   * @param args Command line arguments to the pipeline.
+   */
+  public static void main(String[] args)
+      throws ScriptException, IOException, NoSuchMethodException {
+    UncaughtExceptionLogger.register();
 
-        @ProcessElement
-        public void processElement(ProcessContext context) {
-            context.output(Document.parse(context.element()));
-        }
-    }
+    Options options = PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
+    BigQueryIOUtils.validateBQStorageApiOptionsStreaming(options);
+    run(options);
+  }
 
-    /**
-     * Main entry point for pipeline execution.
-     *
-     * @param args Command line arguments to the pipeline.
-     */
-    public static void main(String[] args)
-            throws ScriptException, IOException, NoSuchMethodException {
-        UncaughtExceptionLogger.register();
+  /** Pipeline to read data from PubSub and write to MongoDB. */
+  public static boolean run(Options options)
+      throws ScriptException, IOException, NoSuchMethodException {
+    options.setStreaming(true);
+    Pipeline pipeline = Pipeline.create(options);
 
-        Options options = PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
-        BigQueryIOUtils.validateBQStorageApiOptionsStreaming(options);
-        run(options);
-    }
+    String inputOption = options.getInputTopic();
+    String jsTransformPath =
+        String.format("gs://hevo_bookipi_dev/%s/transforms/transform.js", options.getCollection());
+    String jsTransformFunctionName = "process";
 
-    /** Pipeline to read data from PubSub and write to MongoDB. */
-    public static boolean run(Options options)
-            throws ScriptException, IOException, NoSuchMethodException {
-        options.setStreaming(true);
-        Pipeline pipeline = Pipeline.create(options);
-
-        String inputOption = options.getInputTopic();
-        String jsTransformPath =
-                String.format("gs://hevo_bookipi_dev/%s/transforms/transform.js", options.getCollection());
-        String jsTransformFunctionName = "process";
-
-        pipeline
-                .apply("Read PubSub Messages", PubsubIO.readStrings().fromTopic(inputOption))
-                .apply(
-                        "RTransform string to document",
-                        ParDo.of(
-                                new DoFn<String, Document>() {
-                                    @ProcessElement
-                                    public void process(ProcessContext c) {
-                                        Document document = Document.parse(c.element());
-                                        c.output(document);
-                                    }
-                                }))
-                .apply(
-                        "UDF",
-                        JavascriptDocumentTransformer.TransformDocumentViaJavascript.newBuilder()
-                                .setFileSystemPath(jsTransformPath)
-                                .setFunctionName(jsTransformFunctionName)
-                                .build())
-                .apply(
-                        "Read and transform data",
-                        ParDo.of(
-                                new DoFn<Document, TableRow>() {
-                                    @ProcessElement
-                                    public void process(ProcessContext c) {
-                                        Document document = c.element();
-                                        TableRow row = BigQueryUtils.getTableSchema(document);
-                                        c.output(row);
-                                    }
-                                }))
-                .apply(
-                        BigQueryIO.writeTableRows()
-                                .to(options.getOutputTableSpec())
-                                .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND));
-        pipeline.run();
-        return true;
-    }
+    pipeline
+        .apply("Read PubSub Messages", PubsubIO.readStrings().fromTopic(inputOption))
+        .apply(
+            "RTransform string to document",
+            ParDo.of(
+                new DoFn<String, Document>() {
+                  @ProcessElement
+                  public void process(ProcessContext c) {
+                    Document document = Document.parse(c.element());
+                    c.output(document);
+                  }
+                }))
+        .apply(
+            "UDF",
+            JavascriptDocumentTransformer.TransformDocumentViaJavascript.newBuilder()
+                .setFileSystemPath(jsTransformPath)
+                .setFunctionName(jsTransformFunctionName)
+                .build())
+        .apply(
+            "Read and transform data",
+            ParDo.of(
+                new DoFn<Document, TableRow>() {
+                  @ProcessElement
+                  public void process(ProcessContext c) {
+                    Document document = c.element();
+                    TableRow row = BigQueryUtils.getTableSchema(document);
+                    c.output(row);
+                  }
+                }))
+        .apply(
+            BigQueryIO.writeTableRows()
+                .to(options.getOutputTableSpec())
+                .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND));
+    pipeline.run();
+    return true;
+  }
 }
-
